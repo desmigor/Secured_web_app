d4db226a6cee0b69e723dfd0da4bb1369552ee24
desmigor <mporeigor@gmail.com>
2022-02-04 02:13:19 +0300

    added everything

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Secured_web_app
+# Secured WebToy
+
+WebToy is a simplified login/signup secured web application. The Front end is developed in HTML, CSS and vanila JS. It's connected to the backend Postgres Database using NodeJS. The database is secured with a password and also...
+
 ### Web App Login Screen
 ![](images/login.png)
 
--- server.js
+++ server.js
@@ -1,22 +1,42 @@
 const {Client} = require('pg')
+const http = require('http');
+const fs = require ('fs');
+const path = require('path');
+
+const hostname = '0.0.0.0';
+const port = 4000;
+
 const client = new Client({
 	user: "postgres",
 	password: "mypass",
-	host: "localhost",
+	host: "postgres",
 	port: 5432,
 	database: "postgres"
 })
 
 client.connect()
-.then(() => console.log("Connected successfuly"))
-.catch(e => console.log(e))
-//.finally(() => client.end())
 
-//Tsting the database connection
-client.query(
-	"INSERT INTO credentials(user_id, email, name, password)VALUES(0, 'hi@gmail.com', 'Tom Hikes', 'strongpass')",
-	(err, res) => {
-	  console.log(err, res);
-	  client.end();
-	}
-);
+server = http.createServer(function(req, res){
+
+    if(req.url === "/"){
+        fs.readFile("./index.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream("./style.css", "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
+
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
